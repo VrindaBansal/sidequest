3613cbfeed5ea4deea62f59b737978bcf71a0149
Vrinda Bansal <50393780+VrindaBansal@users.noreply.github.com>
2025-10-04 21:36:05 -0700

    Initial Commit

---
+++ sidequest/sidequestApp.swift
@@ -0,0 +1,17 @@
+//
+//  sidequestApp.swift
+//  sidequest
+//
+//  Created by vrinda  on 10/4/25.
+//
+
+import SwiftUI
+
+@main
+struct sidequestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
